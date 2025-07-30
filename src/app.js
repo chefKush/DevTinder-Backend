880cee43bafa13b39c636bc69293c22db9754cd3
chefKush <kushalms001@gmail.com>
2025-07-30 11:24:35 +0530

    handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,25 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send('Testing form the server!');
-})
-
-app.get('/user/:userId' , (req , res) => {
-    console.log(req.params);
-    res.send({firstName: 'Kush', lastName: 'Aish'})
-})
-  
-app.post('/user', (req, res) => {
-    res.send({message: 'User created successfully!'});
-})
-
-app.delete('/user', (req, res) => {
-    res.send({message: 'User deleted successfully!'});
-})
-
-app.patch('/user', (req, res) => {
-    res.send({message: 'User updated successfully!'});
+app.use('/user', (req, res , next) => {
+    console.log('Route handler 1');
+    // res.send('Route handler 1 response');
+    next();
+}, (req, res , next) => {
+    console.log('Route handler 2')
+    // res.send('Route handler 2 response');
+    next();
+}, (req, res , next) => {
+    console.log('Route handler 3')
+    res.send('Route handler 3 response');
+    // next();
 })
 
 app.listen(7777 , () => {
